b202e625197237bd151833d7ea86d878459c41f5
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-11-17 07:38:34 +0530

    Create Content.jsx

---
+++ src/pages/Content.jsx
@@ -0,0 +1,11 @@
+import LectureTabs from "../components/LectureTabs";
+
+const Content = () => {
+  return (
+    <div className="p-4">
+      <LectureTabs />
+    </div>
+  );
+};
+
+export default Content;
